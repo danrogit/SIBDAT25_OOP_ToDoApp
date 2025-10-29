7f8adc2bd33ed7f6d7d134be9f66d10493654dde
Casper Christensen <Casper@perb.dk>
2025-10-29 11:13:02 +0100

    Fase 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using SIBDAT25_OOP_ToDoApp.Model;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace SIBDAT25_OOP_ToDoApp
 {
@@ -16,9 +19,54 @@ namespace SIBDAT25_OOP_ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        Opgave opgave = new Opgave();
+        ObservableCollection<Opgave> OpgaveListe { get; set; } = new ObservableCollection<Opgave>();
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            
+            Liste.ItemsSource = OpgaveListe;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // Add button
+        {
+            string NyOpgave = OpgaveFelt.Text;
+
+            if (!string.IsNullOrEmpty(NyOpgave))
+            {
+                OpgaveListe.Add(new Opgave { Opg = NyOpgave, ok = false });
+                OpgaveFelt.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Tilføj Opgave");
+            }
+        }
+
+        private void Opgave_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            Opgave sletOpgave = (Opgave)Liste.SelectedItem;
+
+            if(sletOpgave !=null)
+            {
+                OpgaveListe.Remove(sletOpgave);
+            }
+            else
+            {
+                MessageBox.Show("Slet Opgave");
+            }
+        }
+
+        private void Liste_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Model/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIBDAT25_OOP_ToDoApp.Model
+{
+    class Opgave
+    {
+        public string navn { get; set; }
+        public DateTime dato { get; set; }
+        public string status { get; set; }
+        public string Opg { get; set; }
+        public bool ok { get; set; }
+        public override string ToString() => Opg ?? navn ?? string.Empty;
+    }
+}
